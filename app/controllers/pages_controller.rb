36afc4de1594ed08006d90ec4c11aaa530a047ee
Sumitha Subramanian <sumithas2@yahoo.com>
2017-01-29 04:52:48 +0000

    Adding article title body

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @articles = Article.last(3)
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :articles
+
   root to: 'pages#index'
 
   get 'pages/about'
 
   get 'pages/contact'
-
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
